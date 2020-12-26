b5fda1311d43b262da626057246dd82914e60cb7
Eric-commit <73726390+Eric-commit@users.noreply.github.com>
2020-12-26 16:26:25 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br" >
+<head>
+	<title>GoncalSystem</title>
+</head>
+<body>
+	<form action="envia-email.php">
+	<br>
+		<input type="name" name="name" placeholder="Angela Cristina" required ><br>  <br>
+		<input type="email" name="email" placeholder="angela.cristina@gmail.com" required> <br> <br>
+		<textarea name="mensagem" placeholder="Digite sua mensagem" required></textarea> <br> <br>
+		<input type="submit" name="bottom" value="Enviar" required>
+	</form>
+</body>
+</html>
