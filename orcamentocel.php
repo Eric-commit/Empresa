083097838d955ff725e5aff8fbf97f479161ddf6
Eric-commit <73726390+Eric-commit@users.noreply.github.com>
2021-01-14 22:02:33 -0300

    Create orcamentocel.php

---
+++ orcamentocel.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>GoncalSystem | Orçamento</title>
+    <link rel="shortcut icon" href="logo.png">
+    <link rel="stylesheet" type="text/css" href="indexcel.css">
+
+</head>
+
+<body>
+    <header class="barraprincipal">
+        <a href="indexcel.php" id="logo"><img src="logo.png" class="logo"position="right" width="500px" ></a>
+        <nav>
+
+            <ul class="botoes">
+
+                <li class="botoes-item sobrenos" ><a href="sobrenoscel.php" class="links">Sobre Nós </a></li>
+                <li class="botoes-item orcamento" ><a href="orcamentocel.php" class="links">Orçamento</a></li>
+
+            </ul>
+        </nav>
+
+    </header>
+
+    <div class="container">
+        <form action="envia-email.php" method="POST">
+            <label>Informe seu email:</label><br>
+            <input type="email" name="email" placeholder="luisa.paula@gmail.com"> <br> <br>
+            <label>Informe seu nome/razão social:</label><br>
+            <input type="name" name="name"> <br><br>
+            <label>Digite sua mensagem:</label>
+            <textarea name="mensagem" placeholder="Digite sua mensagem e conte que tipo de site você deseja"></textarea>
+
+        <input type="submit" value="Pedir Orçamento" >
+        </form>
+    </div>
+
+</body>
+
+</html>
