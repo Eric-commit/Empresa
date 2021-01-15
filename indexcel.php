1e4de4ec727cb7828c7905ee23ec51971a7a0cb1
Eric-commit <73726390+Eric-commit@users.noreply.github.com>
2021-01-14 22:02:04 -0300

    Create indexcel.php

---
+++ indexcel.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    
+<head>
+    <title>GoncalSystem</title>
+    <link rel="shortcut icon" href="logo.png">
+    <link rel="stylesheet" type="text/css" href="indexcel.css">
+
+</head>
+
+<body>
+    <header class="barraprincipal">
+        <a href="indexcel.php" id="logo"><img src="logo.png" class="logo" position="right" width="500px" ></a>
+        <nav>
+
+            <ul class="botoes">
+
+                <li class="botoes-item sobrenos" ><a href="sobrenoscel.php" class="links">Sobre Nós </a></li>
+                <li class="botoes-item orcamento" ><a href="orcamentocel.php" class="links">Orçamento</a></li>
+
+            </ul>
+        </nav>
+
+    </header>
+
+    <div class="conteudo">
+        <h1>Criamos seu site profissional com as linguagens de programação mais recentes.</h1>
+    </div>
+
+    <div class="conteudo2">
+
+        <h1>Por que sua empresa precisa de um site?</h1> <br>
+        <p> Atualmente, a influência da internet está cada vez maior. Empresas com plataformas virtuais são mais conhecidas e mais bem apresentadas à seus clientes por disponibilizarem seu catálogo e suas informações de contato e localização na agilidade
+            de um click. </p>
+
+    </div>
+
+    <div class="conteudo3">
+        <h1>Faça já seu orçamento! Com preços que cabém no seu bolso.</h1> <a href="orcamento.html">Orçamento</a>
+    </div>
+
+
+</body>
+
+</html>
